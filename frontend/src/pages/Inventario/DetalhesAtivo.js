8713cc75dce5947a72b5aee3c2dce40594324046
Leandro <proddrk@outlook.com>
2026-02-16 18:55:15 -0300

    fix(frontend): remove unused param in DetalhesAtivo

--- frontend/src/pages/Inventario/DetalhesAtivo.js
+++ frontend/src/pages/Inventario/DetalhesAtivo.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import { Typography } from '@mui/material';
 
 function DetalhesAtivo() {
-  const { id } = useParams();
-
   return (
     <div>
       <Typography variant="h4">Detalhes do Ativo</Typography>
